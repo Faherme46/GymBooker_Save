6311f72128e48e29c004f8af60eb4695141f88bb
Faherme46 <faherme46@gmail.com>
2023-05-17 12:08:01 -0500

    Iniciar Verificacion

--- app/src/main/java/com/example/gymbooker/User/RegisterActivity.java
+++ app/src/main/java/com/example/gymbooker/User/RegisterActivity.java
@@ -84,90 +84,109 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-    public void onClickGuardar(View view){
+    public void onClickGuardar(View view) {
+
+        if(verificar()) {
+
+            HelperPersona bInstance = new HelperPersona();
+            User u = new User();
+            u.setNombre(txtnombre.getText().toString());
+            u.setTelefono(txttelefono.getText().toString());
+            u.setCorreo(txtcorreo.getText().toString());
+            u.setCedula(txtcedula.getText().toString());
+            u.setFechaNacimiento(txtfnacimiento.getText().toString());
+            String t1 = getIntent().getStringExtra("txtToken");
 
 
-        HelperPersona bInstance = new HelperPersona();
-        User u = new User();
-        u.setNombre(txtnombre.getText().toString());
-        u.setTelefono(txttelefono.getText().toString());
-        u.setCorreo(txtcorreo.getText().toString());
-        u.setCedula(txtcedula.getText().toString());
-        u.setFechaNacimiento(txtfnacimiento.getText().toString());
-        String t1=getIntent().getStringExtra("txtToken");
+            HelperToken helperToken = new HelperToken();
+            Tokens token1 = helperToken.getTokenByToken(t1);
+            if (preferences.getString("user", "").equals("admin")) {
+                u.setToken(null);
+                //todo implementar aqui el Registro en google
+
+            BeginSignInRequest.Builder signInRequest;
+            signInRequest = BeginSignInRequest.builder()
+                    .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                            .setSupported(true)
+                            // Your server's client ID, not your Android client ID.
+                            .setServerClientId(getString(R.string.client))
+                            // Only show accounts previously used to sign in.
+                            .setFilterByAuthorizedAccounts(true)
+                            .build());
 
 
-        HelperToken helperToken=new HelperToken();
-        Tokens token1 =helperToken.getTokenByToken(t1);
-        if (preferences.getString("user","").equals("admin")){
-            u.setToken(null);
-            //todo implementar aqui el Registro en google
-//            String correo= u.getCorreo();
-//            BeginSignInRequest.Builder signInRequest;
-//            signInRequest = BeginSignInRequest.builder()
-//                    .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//                            .setSupported(true)
-//                            // Your server's client ID, not your Android client ID.
-//                            .setServerClientId(getString(R.string.client))
-//                            // Only show accounts previously used to sign in.
-//                            .setFilterByAuthorizedAccounts(true)
-//                            .build());
-
-        }else{
-            u.setToken(t1);
-            token1.setUsed(true);
-            //todo cambiar post a update
-            helperToken.postToken(token1);
-        }
-        bInstance.postUser(u);
-        String idToken = null;
-        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(firebaseCredential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d( "creado", "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("TAG", "signInWithCredential:failure", task.getException());
-                            updateUI(null);
+            String idToken = u.getCorreo();
+            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("creado", "signInWithCredential:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("TAG", "signInWithCredential:failure", task.getException());
+                                updateUI(null);
+                            }
                         }
-                        int response = 0;       //Desvinculé que postUser fuese de return int, para poder subirlo al firestore
+                    });
 
-        SharedPreferences.Editor editor= preferences.edit();
-        editor.putInt("logged",1);
-        editor.putString("ccUsuario",u.getCedula());
-        editor.apply();
-        //Actualizar token
+            } else {
+                u.setToken(t1);
+                token1.setUsed(true);
+                //todo cambiar post a update
+                helperToken.postToken(token1);
+            }
+            bInstance.postUser(u);
 
-        Intent i=new Intent(RegisterActivity.this, MainActivity.class);
-        startActivity(i);
-        finish();
+
+            //Desvinculé que postUser fuese de return int, para poder subirlo al firestore
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("logged", 1);
+            editor.putString("ccUsuario", u.getCedula());
+            editor.apply();
+            //Actualizar token
+
+            Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
 //        }else {
 //            Toast.makeText(this, "Error al Conectar", Toast.LENGTH_SHORT).show();
 //            Intent i=new Intent(RegisterActivity.this, LoginActivity.class);
 //            startActivity(i);
 //        }
 
+        }else{
+            Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private boolean verificar(){
+        if(
+        esNulo((EditText) txtnombre) ||
+        esNulo((EditText) txtfnacimiento) ||
+        esNulo((EditText) txtcedula) ||
+        esNulo((EditText) txtcorreo) ||
+        esNulo((EditText) txttelefono) ){
+            return false;
+        }else{
+            //todo las verificaciones de cedula, de telefono, de correo
+        }
+        return true;
 
 
-
-                    }
-                });
     }
 
-    private  void verificar(){
-        esNulo((EditText) txtnombre);
-        //todo las verificaciones de cedula, de telefono, de correo, de contraseña
-    }
-
-    private void esNulo(EditText et) {
+    private boolean esNulo(EditText et) {
         if(et.getText().toString().isEmpty()){
             et.setError("Complete este campo");
+            return false;
+        }else{
+            return true;
         }
+
     }
 }
